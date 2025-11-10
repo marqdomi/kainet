828d1d3eecb67218754c22c66678dc1bf03464f6
Marco Domínguez <marcdomibe@gmail.com>
2025-11-10 10:52:50 -0600

    fix: corregir formulario de contacto - configurar API routes y CORS

--- api/contact.js
+++ api/contact.js
@@ -12,6 +12,20 @@ const isValidEmail = (email) => {
 };
 
 module.exports = async function handler(req, res) {
+  // Configurar CORS headers
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+
+  // Manejar preflight request
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   // Solo permitir POST
   if (req.method !== 'POST') {
     return res.status(405).json({ 
